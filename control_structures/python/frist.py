e3287fd92c13461abded11c5217ef14f5a015714
gantay11 <sanskargurung412@gmail.com>
2025-08-20 23:45:07 +0600

    frist.py

---
+++ control_structures/python/frist.py
@@ -0,0 +1,13 @@
+noOfcandies = [2, 4, 7, 8, 0]
+
+print (noOfcandies[3])
+
+extracandies = 2
+
+maxcandies = max(noOfcandies)
+
+for candies = max(noOfcandies)
+temp = candies + extracandies
+print (temp)
+
+  
